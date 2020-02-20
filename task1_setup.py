b3bbd1655dc6b4e686fd6adb16950567336782f7
McCreanor (P-U6) Hamish <15McCreanorH@harrowschool.org.uk>
2020-02-20 19:18:10 +0000

    Update task1_setup.py

--- task1_setup.py
+++ task1_setup.py
@@ -16,27 +16,23 @@ class book:
     def __eq__(self, b):
         return self.score == b.score
 
+
 class library:
-    def __init__(self, id, signup, booksperday):
+    def __init__(self,signup,booksperday):
         self.signup,self.booksperday = signup,booksperday
         self.books = []
-    
-    def sort_books(self):
-        self.books.sort()
 
 f = open(to_read,"r")
 lines = f.readlines()
 
-for id,score in enumerate(lines[2].split()):
+for id,score in enumerate(lines[1].split()):
     global_books.append(book(id,score))
 
-i = 0
 n = 2
 while n < len(lines):
-    this_library = lines[n:n+3]
+    this_library = lines[n:n+2]
     parms = this_library[0].split()
-    libraries.append(library(int(parms[1]),i,int(parms[2])))
-    for i in this_library[2].split():
+    libraries.append(library(int(parms[1]),int(parms[2])))
+    for i in this_library[1].split():
         libraries[-1].books.append(global_books[int(i)])
-    i += 1
-    n += 2
+        n += 2
