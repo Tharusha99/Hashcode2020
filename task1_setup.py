a129dfa7b45db7bc25044e18bc4f82e123c48961
McCreanor (P-U6) Hamish <15McCreanorH@harrowschool.org.uk>
2020-02-20 19:05:20 +0000

    sntaohu

--- task1_setup.py
+++ task1_setup.py
@@ -1,5 +1,4 @@
-import sys
-to_read = sys.argv[1]
+to_read = "a_example.txt"
 
 global_books = []
 libraries = []
@@ -8,10 +7,22 @@ class book:
     def __init__(self,id,score):
         self.id,self.score=id,score
 
+    def __lt__(self, b):
+        return self.score < b.score
+
+    def __gt__(self, b):
+        return self.score > b.score
+
+    def __eq__(self, b):
+        return self.score == b.score
+
 class library:
-    def __init__(self,signup,booksperday):
+    def __init__(self, id, signup, booksperday):
         self.signup,self.booksperday = signup,booksperday
         self.books = []
+    
+    def sort_books(self):
+        self.books.sort()
 
 f = open(to_read,"r")
 lines = f.readlines()
@@ -19,11 +30,13 @@ lines = f.readlines()
 for id,score in enumerate(lines[2].split()):
     global_books.append(book(id,score))
 
+i = 0
 n = 4
 while n < len(lines):
     this_library = lines[n:n+3]
     parms = this_library[0].split()
-    libraries.append(library(int(parms[1]),int(parms[2])))
+    libraries.append(library(int(parms[1]),i,int(parms[2])))
     for i in this_library[2].split():
         libraries[-1].books.append(global_books[int(i)])
+    i += 1
     n += 3
